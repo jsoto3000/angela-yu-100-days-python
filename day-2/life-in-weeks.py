08997c92c4f3842882dbc6bfb8efcd9fd464be07
J. Soto <jsoto3070@gmail.com>
2020-11-15 21:13:48 -0500

    complete day 3

---
+++ day-2/life-in-weeks.py
@@ -0,0 +1,12 @@
+# Don't change the code below
+age = input("What is your current age? ")
+# Don't change the code above
+
+#Write your code below this line
+
+years = 90 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
