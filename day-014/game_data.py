59df4324015352550217478e3267f7839385e8a4
J. Soto <jsoto3070@gmail.com>
2020-11-30 00:16:58 -0500

    completed day 14

---
+++ day-014/game_data.py
@@ -0,0 +1,515 @@
+marvel_data = [
+
+            {
+                'name': 'Thor',
+                'strength_count': 550,
+                'description': 'Asgardian God of Thunder',
+                'universe': 'Marvel'
+            },
+
+            {
+                'name': 'Hulk',
+                'strength_count': 500,
+                'description': 'Green Gamma Radiated Hero',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Captain America',
+                'strength_count': 250,
+                'description': 'Super Soldier',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Iron Man',
+                'strength_count': 400,
+                'description': 'Armored Avenger',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Spiderman',
+                'strength_count': 350,
+                'description': 'Mutated Human Arachnid',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Moon Knight',
+                'strength_count': 250,
+                'description': 'Resurrected Egyptian Moon God',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Black Widow',
+                'strength_count': 150,
+                'description': 'Trained Assassin',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Submariner',
+                'strength_count': 450,
+                'description': 'Lord of Atlantis',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Black Panther',
+                'strength_count': 375,
+                'description': 'King of Wakanda',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Hawkeye',
+                'strength_count': 100,
+                'description': 'Skilled Archer',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Daredevil',
+                'strength_count': 250,
+                'description': 'Super Sensitive Blind Hero',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Dr. Strange',
+                'strength_count': 550,
+                'description': 'Sorcerer Supreme',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Blade',
+                'strength_count': 275,
+                'description': 'Daywalker Vampire',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Punisher',
+                'strength_count': 275,
+                'description': 'Armed Vigilante',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Captain Marvel',
+                'strength_count': 550,
+                'description': 'Human Kree Super Warrior',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Shang Chi',
+                'strength_count': 250,
+                'description': 'Master of Kung Fu',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Iron Fist',
+                'strength_count': 225,
+                'description': 'Asgardian God of Thunder',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Thing',
+                'strength_count': 525,
+                'description': 'Rocky Fantastic Four Member',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Human Torch',
+                'strength_count': 350,
+                'description': 'Fiery Fantastic Four Member',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Mister Fantastic',
+                'strength_count': 400,
+                'description': 'Genius Fantastic Four Leader',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Black Bolt',
+                'strength_count': 550,
+                'description': 'King of the Inhumans',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Medusa',
+                'strength_count': 450,
+                'description': 'Queen of the Inhumans',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Professor X',
+                'strength_count': 600,
+                'description': 'Supreme X-Men Mutant Psychic',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Phoenix',
+                'strength_count': 450,
+                'description': 'X-Men Mutant Telepath',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Cyclops',
+                'strength_count': 350,
+                'description': 'X-Men Mutant with Eye Beams',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Storm',
+                'strength_count': 375,
+                'description': 'X-Men Mutant Weather Goddess',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Wolverine',
+                'strength_count': 450,
+                'description': 'X-Men Mutant Adamantium Claws',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Beast',
+                'strength_count': 350,
+                'description': 'X-Men Mutant Lead Scientist',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'She Hulk',
+                'strength_count': 350,
+                'description': 'Green Gamma Radiated Heroine',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Luke Cage',
+                'strength_count': 375,
+                'description': 'Power House',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Colossus',
+                'strength_count': 450,
+                'description': 'X-Men Mutant Steel',
+                'universe': 'Marvel'
+            },
+
+
+            {
+                'name': 'Deadpool',
+                'strength_count': 475,
+                'description': 'Mercenary with a Mouth',
+                'universe': 'Marvel'
+            },
+
+]
+
+dc_data = [
+
+            {
+                'name': 'Superman',
+                'strength_count': 600,
+                'description': 'Kryptonian',
+                'universe': 'DC'
+            },
+
+            {
+                'name': 'Batman',
+                'strength_count': 500,
+                'description': 'Detective',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Wonder Woman',
+                'strength_count': 550,
+                'description': 'Amazonian',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Aquaman',
+                'strength_count': 500,
+                'description': 'King of Atlantis',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Mera',
+                'strength_count': 400,
+                'description': 'Queen of Atlantis',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Green Lantern',
+                'strength_count': 450,
+                'description': 'Emerald Knight',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Green Arrow',
+                'strength_count': 150,
+                'description': 'Archer',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'The Flash',
+                'strength_count': 350,
+                'description': 'Speedster',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Black Lightning',
+                'strength_count': 375,
+                'description': 'Electrical Hero',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Static Shock',
+                'strength_count': 400,
+                'description': 'ELectromagentic Master',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Black Canary',
+                'strength_count': 275,
+                'description': 'Vigilante Heroine',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Cyborg',
+                'strength_count': 550,
+                'description': 'Human Robot Supreme',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Vigilante',
+                'strength_count': 275,
+                'description': 'Armed Crime Fighter',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Hawkman',
+                'strength_count': 550,
+                'description': 'Alien Warrior',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Martian Manhunter',
+                'strength_count': 350,
+                'description': 'Alien Detective',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Shazam',
+                'strength_count': 450,
+                'description': 'Magical Power House',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Captain Atom',
+                'strength_count': 700,
+                'description': 'Atomic Power',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Supergirl',
+                'strength_count': 500,
+                'description': 'Kryptonian',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Vibe',
+                'strength_count': 275,
+                'description': 'Vibration',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Hawkgirl',
+                'strength_count': 400,
+                'description': 'Alien Warrior',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'John Constantine',
+                'strength_count': 450,
+                'description': 'Sorcerer',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Etrigan',
+                'strength_count': 350,
+                'description': 'Demon Prince',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Deadman',
+                'strength_count': 325,
+                'description': 'Supernatural Force',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Spectre',
+                'strength_count': 450,
+                'description': 'Cosmic Entity',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Dr. Fate',
+                'strength_count': 350,
+                'description': 'Magical Hero',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Stargirl',
+                'strength_count': 225,
+                'description': 'Patriotic Heroine',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Adam Strange',
+                'strength_count': 275,
+                'description': 'Heroic Scientist',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Robin',
+                'strength_count': 250,
+                'description': 'Sidekick',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Red Hood',
+                'strength_count': 350,
+                'description': 'Killer Vigilante',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Shade the Changing Man',
+                'strength_count': 275,
+                'description': 'Poetic Alterer of Reality',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Detective Chimp',
+                'strength_count': 150,
+                'description': 'Simian Intellectual Genius',
+                'universe': 'DC'
+            },
+
+
+            {
+                'name': 'Swamp Thing',
+                'strength_count': 525,
+                'description': 'Avatar of the Green',
+                'universe': 'DC'
+            },
+
+]
---
+++ day-014/marvel_vs_dc.py
@@ -0,0 +1,88 @@
+from game_data import marvel_data
+from game_data import dc_data
+import random
+from art import logo, vs
+from os_clear import clear
+
+def get_random_marvel_account():
+    """Get data from random account"""
+    return random.choice(marvel_data)
+
+def get_random_dc_account():
+    """Get data from random account"""
+    return random.choice(dc_data)
+
+def format_marvel_data(marvel_account):
+    """Format account into printable format: name, description and universe"""
+    marvel_name = marvel_account["name"]
+    marvel_description = marvel_account["description"]
+    marvel_universe = marvel_account["universe"]
+    # print(f'{marvel_name}: {marvel_account["strength_count"]}')
+    return f"{marvel_name}, the {marvel_description}, from {marvel_universe}"
+
+def format_dc_data(dc_account):
+    """Format account into printable format: name, description and universe"""
+    dc_name = dc_account["name"]
+    dc_description = dc_account["description"]
+    dc_universe = dc_account["universe"]
+    # print(f'{dc_name}: {dc_account["strength_count"]}')
+    return f"{dc_name}, the {dc_description}, from {dc_universe}"
+
+def check_answer(guess, marvel_strength_count, dc_strength_count):
+    """Checks followers against user's guess
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if marvel_strength_count > dc_strength_count:
+        return guess == "marvel"
+    else:
+        return guess == "dc"
+
+
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_marvel = get_random_marvel_account()
+    account_dc = get_random_dc_account()
+
+    while game_should_continue:
+        account_dc = get_random_dc_account()
+        account_marvel = get_random_marvel_account()
+
+        print(f"Compare A: {format_marvel_data(account_marvel)}.")
+        print(vs)
+        print(f"Against B: {format_dc_data(account_dc)}.\n")
+
+        guess = input("Which universe has the strongest hero? Type 'Marvel' or 'DC': ").lower()
+
+        marvel_strength_count = account_marvel["strength_count"]
+        dc_strength_count = account_dc["strength_count"]
+
+        marvel_super_hero = account_marvel["name"]
+        dc_super_hero = account_dc["name"]
+
+
+        if marvel_strength_count == dc_strength_count:
+            is_correct = True
+        else:
+            is_correct = check_answer(guess, marvel_strength_count, dc_strength_count)
+
+        clear()
+        print(logo)
+        if is_correct and marvel_strength_count == dc_strength_count:
+            score += 0
+            print(f"It's a tie! You still get to play. Current score: {score}.\n")
+            print(f'Marvel\'s {marvel_super_hero} strength level is: {marvel_strength_count}.')
+            print(f'DC\'s {dc_super_hero} strength level is: {dc_strength_count}.\n\nPlay again!\n')
+        elif is_correct:
+            score += 1
+            print(f"You win! You get a point. Current score: {score}.\nPlay again!\n")
+            print(f'Marvel\'s {marvel_super_hero} strength level is: {marvel_strength_count}.')
+            print(f'DC\'s {dc_super_hero} strength level is: {dc_strength_count}.\n\nPlay again!\n')
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Game is terminated. Final score: {score}\n")
+            print(f'Marvel\'s {marvel_super_hero} strength level is: {marvel_strength_count}.')
+            print(f'DC\'s {dc_super_hero} strength level is: {dc_strength_count}.\n\nGame Over!\n')
+
+game()
