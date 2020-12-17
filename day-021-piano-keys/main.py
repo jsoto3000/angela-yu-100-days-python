931257d340aad391c7da9bd1b28184650895776b
J. Soto <jsoto3070@gmail.com>
2020-12-16 22:30:40 -0500

    completed day 021

---
+++ day-021-piano-keys/main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuple = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+print(piano_tuple[1:])
