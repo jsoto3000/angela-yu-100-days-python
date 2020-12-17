363215bb7fb3e1a5160ae85dab5a516080db532a
J. Soto <jsoto3070@gmail.com>
2020-12-16 22:16:28 -0500

    complete day 020

---
+++ day-020-snake-1/main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+
+class Food(object):
+    pass
+
+
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
