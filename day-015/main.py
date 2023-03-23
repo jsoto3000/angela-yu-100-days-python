faf321d6472906d008738d51cc52b2696966cffd
J. Soto <jsoto3070@gmail.com>
2023-03-23 17:07:16 -0400

    completed day 15 revisited

--- day-015/main.py
+++ day-015/main.py
@@ -22,7 +22,7 @@ MENU = {
             "water": 50,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 1.50,
     },
     "latte": {
         "ingredients": {
@@ -30,7 +30,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 2.50,
     },
     "cappuccino": {
         "ingredients": {
@@ -38,16 +38,19 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 3.00,
     }
 }
 
-profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+
+profit = 0
 
 
 def is_resource_sufficient(order_ingredients):
@@ -61,7 +64,7 @@ def is_resource_sufficient(order_ingredients):
 
 def process_coins():
     """Returns the total calculated from coins inserted."""
-    print("Please insert coins.")
+    print(f"Your selection costs ${MENU[choice].get('cost'):.2f}. Please insert coins.")
     total = int(input("how many quarters?: ")) * 0.25
     total += int(input("how many dimes?: ")) * 0.1
     total += int(input("how many nickles?: ")) * 0.05
@@ -73,7 +76,7 @@ def is_transaction_successful(money_received, drink_cost):
     """Return True when the payment is accepted, or False if money is insufficient."""
     if money_received >= drink_cost:
         change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change.")
+        print(f"Here is ${change:.2f} in change.")
         global profit
         profit += drink_cost
         return True
@@ -92,14 +95,14 @@ def make_coffee(drink_name, order_ingredients):
 is_on = True
 
 while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${profit}")
+        print(f"Water: {resources['water']:}ml")
+        print(f"Milk: {resources['milk']:}ml")
+        print(f"Coffee: {resources['coffee']:}g")
+        print(f"Money: ${profit:.2f}")
     else:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
